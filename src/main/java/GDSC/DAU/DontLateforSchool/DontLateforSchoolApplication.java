f084b1c5fb5e445af3c9aead23a5001babffcc78
toki0411 <toki402@naver.com>
2023-07-13 22:56:11 +0900

    chore: 프로젝트 생성

---
+++ src/main/java/GDSC/DAU/DontLateforSchool/DontLateforSchoolApplication.java
@@ -0,0 +1,13 @@
+package GDSC.DAU.DontLateforSchool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DontLateforSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DontLateforSchoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/GDSC/DAU/DontLateforSchool/DontLateforSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package GDSC.DAU.DontLateforSchool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DontLateforSchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
