42c7ab48fb36acc53c84813064dc27ab45053512
Hyejin Shin <84217409+toki0411@users.noreply.github.com>
2023-07-18 21:20:05 +0900

    Update DontLateforSchoolApplication.java

--- src/main/java/GDSC/DAU/DontLateforSchool/DontLateforSchoolApplication.java
+++ src/main/java/GDSC/DAU/DontLateforSchool/DontLateforSchoolApplication.java
@@ -1,6 +1,7 @@
 package GDSC.DAU.DontLateforSchool;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
